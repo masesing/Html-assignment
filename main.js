060071a92ea698f8e70a5a4c63aaef6e530a6bc6
masesing <masesing@icloud.com>
2024-04-25 12:20:29 +0200

    third

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var typed = new typed(".text", {
-    string: ["Programmer", "Technician" ,  "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.scroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+            });
+        };
+    });
+}
